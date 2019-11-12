feb161295a2e80175e3e0ea3d2b9251787b1b265
ironijunior <ironimedina@gmail.com>
2019-11-12 16:56:17 -0300

    create main class

---
+++ application/src/main/java/com/ironijunior/energyconsumption/MainApplication.java
@@ -0,0 +1,23 @@
+package com.ironijunior.energyconsumption;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Main entry point of this application.
+ *
+ * @author Ironi Junior Medina
+ */
+@SpringBootApplication
+public class MainApplication {
+
+    /**
+     * Method responsible for the start the Spring application.
+     *
+     * @param args arguments
+     */
+	public static void main(String[] args) {
+		SpringApplication.run(MainApplication.class, args);
+	}
+
+}
